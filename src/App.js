60a11315c4239830b451c1d1d32e4a20cdb068db
Mariem-chlif <chlif.mariem@outlook.com>
2021-04-04 02:16:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react'
+
+let ProfilePhotoStyle = {
+  border: "2px solid white",
+  padding: "10px",
+  borderRadius: "50px 20px",
+  width: "200px",
+  height: "200px"
+}
+class App extends Component {
+  constructor(props){
+    super (props) ;
+    this.state= {
+     Person:{fullName : "Mariem chlif" ,
+      bio : " Highly motivated sales executive with extensive customer service and sales experience ,worked for 3 years at TUI Tunisia  ",
+      imgSrc : <img src="/photo.png"  style={ProfilePhotoStyle}  alt="my_profile" /> ,
+      profession :  "SALES EXECUTIVE "
+    },
+      show :false ,
+      count: 0
+    };
+  }
+  showName =() => {
+      this.setState({show :!this.state.show})
+  }
+
+  componentDidMount() {
+    this.myInterval = setInterval (()=>{
+      this.setState(prevState => ({
+        count: prevState.count + 1
+    }))
+  }, 5000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.myInterval)
+  }
+
+
+
+  render() {
+    const count = this.state.count
+    return (
+      <div className="profil" >
+      { this.state.show ? (<div> {this.state.Person.imgSrc} <br/>
+        <h1> {this.state.Person.fullName} </h1>   <br/>
+       <p>  {this.state.Person.profession}  </p> <br/>
+       <p> {this.state.Person.bio} </p> </div>):null  }  
+
+        <button onClick= {this.showName} > {this.state.show}Show profile </button>
+
+        <h1>Current count : {count} </h1>
+      
+      </div>
+    )
+  }
 }
 
 export default App;
